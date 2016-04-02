637b8887c3bf3388e984fe586c04a9d9d98aba49
Surrui <Surrui@HAL9000>
2016-04-02 23:05:07 +0200

    Commit: Conexión con la base de datos realizada

--- Reptils/src/main/java/net/javierjimenez/Reptils/ReptilsController.java
+++ Reptils/src/main/java/net/javierjimenez/Reptils/ReptilsController.java
@@ -1,7 +1,15 @@
 package net.javierjimenez.Reptils;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import java.sql.Statement;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -37,9 +45,24 @@ public class ReptilsController implements Initializable {
 	@FXML
 	private ComboBox<String> estatAnimal;
 	
+	private Statement animals = null;
+	
+	private Connection conn = null;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://192.168.4.1/traductor", "foot", "ball");
+			animals = conn.createStatement();
+		} catch (SQLException e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Problema de conexión con BD");
+			alert.setHeaderText("ERROR: Problema de conexión");
+			alert.setContentText("Error al conectar con la base de datos.\nRevise si esta operativa.");
+
+			alert.showAndWait();
+
+			System.exit(0);
+		}
 	}
 }

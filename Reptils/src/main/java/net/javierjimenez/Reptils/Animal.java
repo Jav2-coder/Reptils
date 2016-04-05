55280a2863b6142242bd0fdb82bfd8727a017012
Surrui <Surrui@HAL9000>
2016-04-06 01:37:23 +0200

    Commit: El programa funciona, empaqueta correctamente y esta documentado

--- Reptils/src/main/java/net/javierjimenez/Reptils/Animal.java
+++ Reptils/src/main/java/net/javierjimenez/Reptils/Animal.java
@@ -1,77 +1,141 @@
 package net.javierjimenez.Reptils;
 
+/**
+ * 
+ * @author Surrui
+ *
+ */
 public class Animal {
 
-	private int codi;
+	/**
+	 * Variable String que conte el nom del Animal
+	 */
 	private String nom;
-	private int ordre;
+	
+	/**
+	 * Variable String que conte la especie del Animal
+	 */
 	private String especie;
+	
+	/**
+	 * Variable String que conte la descripcio del Animal
+	 */
 	private String descripcio;
+	
+	/**
+	 * Variable String que conte l'estat del Animal
+	 */
 	private String estat;
+	
+	/**
+	 * Variable String que conté la url de la imatge del Animal
+	 */
 	private String imatge;
 	
-	public Animal(int code, String name, int order, String species, String description, String state, String image) {
-		this.codi = code;
+	/**
+	 * Constructor principal del objecte Animal
+	 * 
+	 * @param name variable String
+	 * @param species variable String
+	 * @param description variable String
+	 * @param state variable String
+	 * @param image variable String
+	 */
+	public Animal(String name, String species, String description, String state, String image) {
+		
 		this.nom = name;
-		this.ordre = order;
 		this.especie = species;
 		this.descripcio = description;
 		this.estat = state;
 		this.imatge = image.replace("://", ":");
 	}
 	
-	public int getCodi() {
-		return codi;
-	}
-
-	public void setCodi(int codi) {
-		this.codi = codi;
-	}
-
+	/**
+	 * Metode que retorna el nom de l'objecte Animal.
+	 * 
+	 * @return
+	 */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	 * Metode que modifica la variable nom de l'objecte Animal
+	 * 
+	 * @param nom Variable String
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-	public int getOrdre() {
-		return ordre;
-	}
-
-	public void setOrdre(int ordre) {
-		this.ordre = ordre;
-	}
-
+	/**
+	 * Metode que retorna la especie de l'objecte Animal.
+	 * 
+	 * @return
+	 */
 	public String getEspecie() {
 		return especie;
 	}
 
+	/**
+	 * Metode que modifica la variable especie de l'objecte Animal
+	 * 
+	 * @param especie Variable String
+	 */
 	public void setEspecie(String especie) {
 		this.especie = especie;
 	}
 
+	/**
+	 * Metode que retorna la descripcio de l'objecte Animal.
+	 * 
+	 * @return
+	 */
 	public String getDescripcio() {
 		return descripcio;
 	}
 
+	/**
+	 * Metode que modifica la variable descripcio de l'objecte Animal
+	 *  
+	 * @param descripcio Variable String
+	 */
 	public void setDescripcio(String descripcio) {
 		this.descripcio = descripcio;
 	}
 
+	/**
+	 * Metode que retorna el estat de l'objecte Animal.
+	 * 
+	 * @return
+	 */
 	public String getEstat() {
 		return estat;
 	}
 
+	/**
+	 * Metode que modifica la variable estat de l'objecte Animal
+	 * 
+	 * @param estat Variable String
+	 */
 	public void setEstat(String estat) {
 		this.estat = estat;
 	}
 
+	/**
+	 * Metode que retorna l'enllaç de la imatge de l'objecte Animal.
+	 * 
+	 * @return
+	 */
 	public String getImatge() {
 		return imatge;
 	}
 
+	/**
+	 * Metode que modifica la variable imatge de l'objecte Animal
+	 * 
+	 * @param imatge Variable String
+	 */
 	public void setImatge(String imatge) {
 		this.imatge = imatge;
 	}
--- Reptils/src/main/java/net/javierjimenez/Reptils/ReptilsController.java
+++ Reptils/src/main/java/net/javierjimenez/Reptils/ReptilsController.java
@@ -23,49 +23,139 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * 
+ * @author Surrui
+ *
+ */
 public class ReptilsController implements Initializable {
+
+	/**
+	 * Objecte ImageView que conte la imatge de l'animal que es mostra
+	 * actualment.
+	 */
 	@FXML
 	private ImageView imgAnimal;
+
+	/**
+	 * Objecte TextArea que conte la descripcio de l'animal que es mostra
+	 * actualment.
+	 */
 	@FXML
 	private TextArea descripcio;
+
+	/**
+	 * Objecte TextField que conte el nom de l'animal que es mostra actualment.
+	 */
 	@FXML
 	private TextField nomAnimal;
+
+	/**
+	 * Objecte TextField que conte la especie de l'animal que es mostra
+	 * actualment.
+	 */
 	@FXML
 	private TextField especieAnimal;
+
+	/**
+	 * Objecte Button encarregat de guardar els canvis que s'efectuen en les
+	 * dades del animal.
+	 */
 	@FXML
 	private Button guardar;
+
+	/**
+	 * Objecte Button que permet accedir al anterior animals d'un mateix ordre
+	 * en cada clic.
+	 */
 	@FXML
 	private Button animalAnt;
+
+	/**
+	 * Objecte Button que permet accedir al següent animals d'un mateix ordre en
+	 * cada clic.
+	 */
 	@FXML
 	private Button animalSeg;
+
+	/**
+	 * Objecte ComboBox que conte els noms de les families de la base de dades
+	 */
 	@FXML
 	private ComboBox<String> familia;
+
+	/**
+	 * Objecte ComboBox que conte els noms dels ordres de dins la base de dades
+	 * segons quina familia s'ha escollit.
+	 */
 	@FXML
 	private ComboBox<String> ordre;
+
+	/**
+	 * Objecte ComboBox que conte els estats de dins la base de dades i mostra
+	 * l'estat del animal que es mostra actualment.
+	 */
 	@FXML
 	private ComboBox<String> estatAnimal = new ComboBox<>();
 
+	/**
+	 * Objecte Statement que executará les sentencies SQL i les envia a la Base
+	 * de Dades.
+	 */
 	private Statement beasts = null;
 
+	/**
+	 * Objecte Connection que conecta el programa a la nostre base de dades.
+	 */
 	private Connection conn = null;
 
+	/**
+	 * Variable String que utilitzarem per emmagatzemar la familia que hem
+	 * escollit
+	 */
 	private String family = null;
 
+	/**
+	 * Variable String que utilitzarem per emmagatzemar l'ordre que hem escollit
+	 */
 	private String ord = null;
 
+	/**
+	 * Variable int que es fara servir per navegar entre els animals que hi ha
+	 * en cada ordre.
+	 */
 	private int index;
 
+	/**
+	 * Objecte List que conte els objectes Animal que creem a partir de les
+	 * dades de la base de dades.
+	 */
 	private List<Animal> animals = new ArrayList<Animal>();
-	
+
+	/**
+	 * Objecte ResultSet
+	 */
 	private ResultSet a;
 
+	/**
+	 * Metode que inicialitza la connexio amb la Base de Dades i que emplena el
+	 * ComboBox familia amb els valors obtinguts de la mateixa.
+	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
+		ResultSet animalFamily;
+
 		try {
-			//conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/reptils", "foot", "ball");
-			conn = DriverManager.getConnection("jdbc:mysql://172.17.0.2:3306/reptils", "foot", "ball");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/reptils", "foot", "ball");
 			beasts = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+			animalFamily = beasts.executeQuery("SELECT nom FROM families");
+
+			while (animalFamily.next()) {
+				familia.getItems().add(animalFamily.getString("nom"));
+			}
+
 		} catch (SQLException e) {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("Problema de conexión con BD");
@@ -75,35 +165,39 @@ public class ReptilsController implements Initializable {
 
 			System.exit(0);
 		}
-		familia.getItems().addAll("Amfibis", "Rèptils");
 
 		animalAnt.setDisable(true);
 		animalSeg.setDisable(true);
 		guardar.setDisable(true);
 	}
 
+	/**
+	 * Metode encarregat de obtindre les dades dels animals de la base de dades
+	 * segons quin ordre hem escollit del ComboBox.
+	 * 
+	 * @param event
+	 */
 	public void seleccionarOrdre(ActionEvent event) {
 
 		if (ordre.getItems() != null && !ordre.getItems().isEmpty()) {
 
 			index = 0;
-			
+
 			guardar.setDisable(false);
-			
+
 			generarEstats();
-			
+
 			animals.clear();
 
 			ord = ordre.getValue().toString();
 
 			try {
-				a = beasts.executeQuery("SELECT * FROM animals WHERE ordre = "
-						+ "(SELECT codi from ordres WHERE nom = '" + ord + "')");
+				a = beasts.executeQuery(
+						"SELECT * FROM animals WHERE ordre = " + "(SELECT codi from ordres WHERE nom = '" + ord + "')");
 
 				while (a.next()) {
-					Animal A = new Animal(a.getInt("codi"), a.getString("nom"), a.getInt("ordre"),
-							a.getString("especie"), a.getString("descripcio"), a.getString("estat"),
-							a.getString("imatge"));
+					Animal A = new Animal(a.getString("nom"), a.getString("especie"), a.getString("descripcio"),
+							a.getString("estat"), a.getString("imatge"));
 					animals.add(A);
 				}
 
@@ -127,29 +221,31 @@ public class ReptilsController implements Initializable {
 		}
 	}
 
-	public void generarEstats(){
-		
-		estatAnimal.getItems().addAll(
-				"Extinta",
-				"Extinta en estat salvatge",
-				"En perill greu",
-				"En perill",
-				"Vulnerable",
-				"Depèn de la conservació",
-				"Gairebé amenaçada",
-				"Risc mínim",
-				"No avaluada");
+	/**
+	 * Metode encarregat d'introduir les diferents opcions dins del ComboBox
+	 * estatAnimal
+	 */
+	public void generarEstats() {
+
+		estatAnimal.getItems().addAll("Extinta", "Extinta en estat salvatge", "En perill greu", "En perill",
+				"Vulnerable", "Depèn de la conservació", "Gairebé amenaçada", "Risc mínim", "No avaluada");
 	}
-	
+
+	/**
+	 * Metode encarregat de obtindre les dades dels ordres dels animals de la
+	 * base de dades segons quina familia hem escollit del ComboBox.
+	 * 
+	 * @param event
+	 */
 	public void seleccionarFamilia(ActionEvent event) {
 
 		animalAnt.setDisable(true);
 		animalSeg.setDisable(true);
 		guardar.setDisable(true);
-		
+
 		ordre.getItems().clear();
 		estatAnimal.getItems().clear();
-		
+
 		descripcio.setText("");
 		nomAnimal.setText("");
 		especieAnimal.setText("");
@@ -174,6 +270,12 @@ public class ReptilsController implements Initializable {
 		}
 	}
 
+	/**
+	 * Metode que recorre l'objecte List cap endevant i mostra les dades del
+	 * seguent animal.
+	 * 
+	 * @param event
+	 */
 	public void segAnimal(ActionEvent event) {
 
 		if (animals.size() > 1) {
@@ -197,6 +299,12 @@ public class ReptilsController implements Initializable {
 		}
 	}
 
+	/**
+	 * Metode que recorre l'objecte List cap enrere i mostra les dades del
+	 * animal anterior.
+	 * 
+	 * @param event
+	 */
 	public void antAnimal(ActionEvent event) {
 
 		if (index != 0) {
@@ -219,52 +327,58 @@ public class ReptilsController implements Initializable {
 			}
 		}
 	}
-	
-	public void actualizarDatos(ActionEvent event){
-		boolean name, description, species, state; 
-		
+
+	/**
+	 * Metode encarregat d'actualitzar les dades en l'objecte List i en la Base
+	 * de Dades, pero nomes en el cas de que es faci algun canvi.
+	 * 
+	 * @param event
+	 */
+	public void actualizarDatos(ActionEvent event) {
+		boolean name, description, species, state;
+
 		name = nomAnimal.getText().equals(animals.get(index).getNom());
 		description = descripcio.getText().equals(animals.get(index).getDescripcio());
 		species = especieAnimal.getText().equals(animals.get(index).getEspecie());
 		state = estatAnimal.getValue().equals(animals.get(index).getEstat());
-		
-		if(!name || !description || !species || !state){
-			
+
+		if (!name || !description || !species || !state) {
+
 			try {
-				
+
 				a.absolute(index + 1);
-				
+
 				a.updateString("nom", nomAnimal.getText());
 				a.updateString("especie", especieAnimal.getText());
 				a.updateString("descripcio", descripcio.getText());
 				a.updateString("estat", estatAnimal.getValue());
-				
+
 				a.updateRow();
-				
+
 				animals.get(index).setNom(nomAnimal.getText());
 				animals.get(index).setEspecie(especieAnimal.getText());
 				animals.get(index).setDescripcio(descripcio.getText());
 				animals.get(index).setEstat(estatAnimal.getValue());
-				
+
 				Alert alert = new Alert(AlertType.INFORMATION);
 				alert.setTitle("Actualización de la BD");
 				alert.setHeaderText("Success!");
 				alert.setContentText("Los datos se han actualizado correctamente");
 				alert.showAndWait();
-				
+
 			} catch (SQLException e) {
-				
+
 				e.printStackTrace();
 			}
-			
+
 		} else {
-			
+
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.setTitle("WARNING: Actualización BD");
 			alert.setHeaderText("Warning!");
 			alert.setContentText("No hay cambios en los datos\npara generar la actualización.");
 			alert.showAndWait();
-			
-		}	
+
+		}
 	}
 }
